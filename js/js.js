95ce0ef64c23ea5e083b93cf800fceeb7c203f7f
DmitriNaumov <dmitr9628@gmail.com>
2022-06-02 19:33:23 +0300

    Skew эффект
    Добавлен skew эффект при скролле блока

--- js/js.js
+++ js/js.js
@@ -19,19 +19,38 @@ $(function () {
         let slHeight = $('.gsap_slider').outerHeight(true);
         let startScrollTrig = (winHeight - slHeight) / 2;
 
+        // Skew
+        let proxy = { skew: 0 },
+            skewSetter = gsap.quickSetter(".gsap__item", "skewX", "deg"),
+            clamp = gsap.utils.clamp(-1000, 1000);
+
         gsap.to(".gsap__track", {
             scrollTrigger: {
                 trigger: ".gsap_slider",
                 // start: "top center",
                 start: () => "-=" + startScrollTrig,
-                end: "+=1000px",
+                end: "+=1500px",
                 // end: () => '+=' + gsapTrack,
                 scrub: true,
-                pin: true
+                pin: true,
                 // markers: true
+                onUpdate: (self) => {
+                    let skew = clamp(self.getVelocity() / 800);
+                    if (Math.abs(skew) > Math.abs(proxy.skew)) {
+                        proxy.skew = skew;
+                        gsap.to(proxy, {
+                            skew: 0,
+                            duration: 0.8,
+                            ease: "power3",
+                            overwrite: true,
+                            onUpdate: () => skewSetter(proxy.skew)
+                        });
+                    }
+                }
             },
-            x: "-" + scrollSliderTransform + "px"
+            x: "-" + scrollSliderTransform + "px",
         });
+        gsap.set(".gsap__item", { transformOrigin: "center center", force3D: true });
 
         setTimeout(() => {
             ScrollTrigger.refresh();
